5340f4e15b0a4679f412f8ce3b0f3791e113da74
Mircea <105327253+MirceaGutium@users.noreply.github.com>
2022-05-13 00:46:04 +0300

    Add files via upload

---
+++ Homework3.java
@@ -0,0 +1,13 @@
+package Homework3;
+
+public class Homework3 {
+    public static void main(String[] args) {
+
+        System.out.println(0);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+    }
+}
