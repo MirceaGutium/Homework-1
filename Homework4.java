a42185bca044fd928580e817fb1c678f4168a09a
Mircea <105327253+MirceaGutium@users.noreply.github.com>
2022-05-13 00:46:16 +0300

    Add files via upload

---
+++ Homework4.java
@@ -0,0 +1,10 @@
+package Homework4;
+
+public class Homework4 {
+    public static void main(String[] args) {
+
+        System.out.println("We need to");
+        System.out.println("Learn Java");
+        System.out.println("As quickly as possible");
+    }
+}
